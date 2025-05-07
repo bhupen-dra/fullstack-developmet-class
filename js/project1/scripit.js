ef9dcfa396b962b94a7bc2e67adba2ac4ad68f47
bhupen-dra <i.am.bhup3ndra@gmail.com>
2025-05-07 10:15:40 +0545

    project update

---
+++ js/project1/scripit.js
@@ -0,0 +1,13 @@
+function glass() {
+    const nam = document.getElementById("name");
+    const iname = document.getElementById("iname");
+    iname.innerHTML = nam.value;
+    const mail = document.getElementById("email");
+    const iemail = document.getElementById("iemail");
+    iemail.innerHTML = mail.value;
+    const parent = document.getElementById("parent");
+    parent.style = "display:block;";
+
+    
+    
+}
